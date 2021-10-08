6850a4c1582cc94d8f17667bc7da04b2fe76a2aa
DeGraw <60065230+degraw4@users.noreply.github.com>
2021-10-08 22:59:35 +0800

    Add files via upload

---
+++ chapter_1/gcc/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// gcc 1.c 2.c
+
+// gcc -c 1.c 2.c
+// gcc 1.o 2.o
+
+int main(int argc, char** argv)
+{
+    printf("hello world\n");
+    fucntion();
+
+    return 0;
+}
---
+++ chapter_1/gcc/2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void fucntion()
+{
+    printf("function\n");
+}
