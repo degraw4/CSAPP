99bfa95d032831cba33606cab8debd2f3f286a3d
DeGraw <60065230+degraw4@users.noreply.github.com>
2021-10-15 17:58:22 +0800

    Add files via upload

---
+++ chapter_7/foo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void f();
+
+int y = 1;
+int x = 2;
+
+int main(int argc, char** argv)
+{
+    f();
+    printf("x = 0x%x, y = ox%x\n", x, y);
+    return 0;
+
+    return 0;
+}
